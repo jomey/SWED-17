533fc5363b001536f20d7b3d09c2aba34e561220
Joachim Meyer <jmeyer@boisestate.edu>
2024-09-17 13:38:31 -0500

    DB functions - CU; Add spatial select
    Add the spatial select to only return centroid points within
    the requested zone. Simplifies function return and calls to it
    (as seen with the view example)

--- dbeaver/Scripts/008-create_cu_boulder-function.sql
+++ dbeaver/Scripts/008-create_cu_boulder-function.sql
@@ -1,7 +1,7 @@
 -- Function to extract zonal CBRFC SWE for CU Boulder SWE rasters
 DROP FUNCTION public.cu_swe_for_zone_and_date;
 CREATE OR REPLACE FUNCTION public.cu_swe_for_zone_and_date(zone_name TEXT, target_date TEXT)
- RETURNS TABLE(swe double precision, raster_center geometry, zone_geom geometry)
+ RETURNS TABLE(swe double precision, raster_center geometry)
  LANGUAGE sql
 AS $function$
     WITH cbrfc_zone AS (
@@ -17,7 +17,8 @@ AS $function$
         FROM cu_boulder cub, cbrfc_zone
         WHERE cub.swe_date = TO_DATE(target_date, 'YYYY-MM-DD')
     )
-    SELECT cu_pixels.val, cu_pixels.geom, cbrfc_zone.geom
+    SELECT cu_pixels.val, cu_pixels.geom
     FROM cu_pixels, cbrfc_zone
+    WHERE ST_WITHIN(cu_pixels.geom, cbrfc_zone.geom)
 $function$
 ;
