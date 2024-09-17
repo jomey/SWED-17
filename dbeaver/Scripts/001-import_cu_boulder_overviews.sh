ad6be6dae4ff0f81f0d3a552e81e382ac62bc022
Joachim Meyer <jmeyer@boisestate.edu>
2024-09-17 12:15:41 -0500

    DB scripts - Add CU Boulder datasets
    Add script that converts the CU Boulder tiffs to COGs. A second step
    updates the created new table with a date column to query for specific
    dates.

---
+++ dbeaver/Scripts/001-import_cu_boulder_overviews.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Convert to tiled raster
+gdal_translate -co TILED=YES -co COMPRESS=ZTD -co PREDICTOR=2 -co NUM_THREADS=ALL_CPUS \
+    ${1} ${1/.tif/_db.tif}
+
+# Import metadata to database
+raster2pgsql -I -C -M -F -R -Y -s 4269 -F -t 32x32 \
+    ${1/.tif/_db.tif} cu_boulder | \
+    psql -U oper -h mujeres -d swe_data
+
+# Add date column with index based on filenames
+psql -U oper -h mujeres -d swe_data -f 001-update_cu_boulder.sql
