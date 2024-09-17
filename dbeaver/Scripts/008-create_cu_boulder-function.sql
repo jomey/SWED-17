6f24004625d5930c34718a9e74578e3d330c8fdc
Joachim Meyer <jmeyer@boisestate.edu>
2024-09-17 12:22:41 -0500

    DB scripts - Add function to query CU Boulder SWAN
    Add SQL function to extract CU Boulder SWE for a given date. This
    pixel extraction logic changed to use the centroid of a pixel to
    determine in which CBRFC zone it belongs to. This removes creating
    more SWE than the product has.

---
+++ dbeaver/Scripts/008-create_cu_boulder-function.sql
@@ -0,0 +1,23 @@
+-- Function to extract zonal CBRFC SWE for CU Boulder SWE rasters
+DROP FUNCTION public.cu_swe_for_zone_and_date;
+CREATE OR REPLACE FUNCTION public.cu_swe_for_zone_and_date(zone_name TEXT, target_date TEXT)
+ RETURNS TABLE(swe double precision, raster_center geometry, zone_geom geometry)
+ LANGUAGE sql
+AS $function$
+    WITH cbrfc_zone AS (
+        select geom, buffered_envelope from public.cbrfc_zone_buffer(zone_name)
+    ),
+    cu_pixels AS (
+        SELECT (ST_PixelAsCentroids(
+            ST_CLIP(
+                cub.rast,
+                cbrfc_zone.buffered_envelope
+            )
+        )).*
+        FROM cu_boulder cub, cbrfc_zone
+        WHERE cub.swe_date = TO_DATE(target_date, 'YYYY-MM-DD')
+    )
+    SELECT cu_pixels.val, cu_pixels.geom, cbrfc_zone.geom
+    FROM cu_pixels, cbrfc_zone
+$function$
+;
