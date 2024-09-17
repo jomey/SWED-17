e1c64d709e8de587098f80adae37bb1a88ab5bbe
Joachim Meyer <jmeyer@boisestate.edu>
2024-09-17 14:57:07 -0500

    DB - Save example to calculate pixel percentages
    Save example query to calcluate the percentage of CBRFC zone
    contained within a SWE pixel.

---
+++ dbeaver/Scripts/pixel_percentage.sql
@@ -0,0 +1,25 @@
+-- Calculate percentage of cbrfc area within a SWE pixel
+-- UA SWE example
+CREATE OR REPLACE VIEW public.UA_ALEC2HLF_PERC AS (
+    WITH cbrfc_zone AS (
+        select geom, buffered_envelope from public.cbrfc_zone_buffer('ALEC2HLF')
+    ),
+    swann_pixels AS (
+        SELECT
+            (ST_PixelAsPolygons(
+                ST_CLIP(
+                    ssm.rast,
+                    cbrfc_zone.buffered_envelope
+                ),
+                1,
+                FALSE
+            )).*
+        FROM swann_swe_mask ssm, cbrfc_zone
+    )
+    SELECT swann_pixels.val, swann_pixels.geom, 
+        ST_Area(ST_Intersection(swann_pixels.geom, cbrfc_zone.geom))/ST_Area(swann_pixels.geom) AS overlap,
+        ST_Area(swann_pixels.geom) AS pixel,
+        ST_Area(cbrfc_zone.geom) AS zone_area
+    FROM cbrfc_zone, swann_pixels
+    WHERE ST_Intersects(swann_pixels.geom, cbrfc_zone.geom)
+);
