e367dedd53983ab0eb4eaa87c0b132d5cc248371
Joachim Meyer <jmeyer@boisestate.edu>
2024-09-24 14:33:41 -0500

    DB - import - Ensure CU SWE is in EPSG:4269
    Also update the table import to not use a extent constraint

--- dbeaver/data_import/003-cu_boulder.sh
+++ dbeaver/data_import/003-cu_boulder.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
+set -e
 
-# Convert to tiled raster
-gdal_translate -co TILED=YES -co COMPRESS=ZTD -co PREDICTOR=2 -co NUM_THREADS=ALL_CPUS \
-    ${1} ${1/.tif/_db.tif}
+# Convert to EPSG:4269
+gdalwarp -overwrite -t_srs EPSG:4269 ${1} ${1/.tif/.vrt}
+
+# Convert to tiled tif
+gdal_translate -co TILED=YES -co COMPRESS=ZSTD -co PREDICTOR=2 -co NUM_THREADS=ALL_CPUS \
+    ${1/.tif/.vrt} ${1/.tif/_db.tif}
 
 # Import metadata to database
-raster2pgsql -I -C -M -F -R -Y -s 4269 -F -t 32x32 \
-    ${1/.tif/_db.tif} cu_boulder | \
+raster2pgsql -d -I -C -x -M -F -R -s 4269 -F -t 32x32 \
+    $(realpath ${1/.tif/_db.tif}) cu_boulder | \
     psql -U oper -h mujeres -d swe_data
 
 # Add date column with index based on filenames
