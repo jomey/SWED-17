5449a42ebd5b43120ff9ab1b1e1ea0416c046c4a
Joachim Meyer <jmeyer@boisestate.edu>
2024-10-08 13:10:36 -0500

    DB - Scripts - Create general DB import script
    Create reusable db import script across SWE import processes.

---
+++ dbeaver/data_import/import_to_db.sh
@@ -0,0 +1,78 @@
+#!/usr/bin/env bash
+#
+# General purpose script to import data to the database
+#
+# Arguments:
+#   -f File to import
+#   -t DB Table to use
+#   -c Create table and import records
+#   -a Append records to table (Default)
+#   -p Post import script to run (Optional)
+#   --out-db: Import file as out-db raster
+
+set -e
+
+DB_CONNECT_OPTIONS='-h mujeres -U oper -d swe_data'
+# Treat files as out-db or import the actual data
+OUT_DB_RASTER=""
+
+# Default import mode is to append
+# Values are raster2pgsql Options
+APPEND_RECORDS="-a"
+CREATE_TABLE="-d -C -x -I"
+IMPORT_MODE=$APPEND_RECORDS
+
+# Script argument options
+LONG_OPTIONS=file:,table:,create,out-db,append,post-import:
+SHORT_OPTIONS=f:t:cap:
+
+# Parse and set variables
+ARGUMENTS=$(getopt --options=${SHORT_OPTIONS} --longoptions=${LONG_OPTIONS} --name $0 -- "$@") || exit 1
+eval set -- "$ARGUMENTS"
+
+while true; do
+    case "$1" in
+        -a|--append)
+            IMPORT_MODE=$APPEND_RECORDS
+            shift
+            ;;
+        -c|--create)
+            IMPORT_MODE=$CREATE_TABLE
+            shift
+            ;;
+        -f|--file)
+            FILE="$2"
+            shift 2
+            ;;
+        -t|--table)
+            TABLE="$2"
+            shift 2
+            ;;
+        -p|--post-import)
+            POST_IMPORT="$2"
+            shift 2
+            ;;
+        --out-db)
+            OUT_DB_RASTER="-R"
+            shift
+            ;;
+        --)
+            shift
+            break
+            ;;
+        *)
+            echo "Error parsing options"
+            exit 1
+            ;;
+    esac
+done
+
+# Import metadata to database
+raster2pgsql ${IMPORT_MODE} -M -F ${OUT_DB_RASTER} -Y -s 4269 -F -t 32x32 \
+    $(realpath ${FILE}) ${TABLE} | \
+    psql ${DB_CONNECT_OPTIONS}
+
+# Process post import script
+if [[ ! -z $POST_IMPORT ]]; then
+    psql ${DB_CONNECT_OPTIONS} -f ${POST_IMPORT}
+fi
