38b89370a584e62edc845615b0cd55a3d503ab7b
Joachim Meyer <jmeyer@boisestate.edu>
2024-10-08 08:35:28 -0500

    DB - Scripts - UA SWE remove tepmorary .vrt file

--- dbeaver/data_import/002-ua_swe_1km.sh
+++ dbeaver/data_import/002-ua_swe_1km.sh
@@ -10,6 +10,9 @@ gdalwarp -overwrite -t_srs EPSG:4269 ${1} ${1/.tif/.vrt}
 gdal_translate -co TILED=YES -co COMPRESS=ZSTD -co PREDICTOR=2 -co NUM_THREADS=ALL_CPUS \
     ${1/.tif/.vrt} ${1/.tif/_db.tif}
 
+# Remove converted vrt file
+rm ${1/.tif/.vrt}
+
 # Import metadata to database
 raster2pgsql -I -C -x -M -F -R -s 4269 -F -t 32x32 \
     $(realpath ${1/.tif/_db.tif}) ua_1km | \
