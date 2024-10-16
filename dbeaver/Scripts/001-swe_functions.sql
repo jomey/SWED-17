a679cb63c86fe263ce9e39a73a4d1cae6cc9f1e0
Joachim Meyer <jmeyer@boisestate.edu>
2024-10-16 11:04:11 -0500

    DB - SWE function - Use SRID of raster
    Refactor the SWE function and transform the CBRFC zone into the
    SRID stored with the raster. This flips the approach of transforming
    all rasters into the same projection before importing them and reduces
    one step of the import.

--- dbeaver/Scripts/001-swe_functions.sql
+++ dbeaver/Scripts/001-swe_functions.sql
@@ -14,6 +14,11 @@ AS $function$
 $function$;
 
 -- Function to query a SWE product for given zone and date
+-- Query steps:
+--  1. Get SRID of target raster to clip from (raster_srid)
+--  2. Get target CBRFC zone and transform to SRID from target raster (cbrfc_zone)
+--  3. Get centroids for each raster pixel that falls within the CBRFC zone (product_pixels)
+--  4. Get values for the pixels (final query)
 DROP FUNCTION IF EXISTS public.swe_from_product_for_zone_and_date;
 CREATE FUNCTION public.swe_from_product_for_zone_and_date(product TEXT, zone_name TEXT, target_date TEXT)
  RETURNS TABLE(swe double precision, raster_center geometry)
@@ -21,25 +26,34 @@ CREATE FUNCTION public.swe_from_product_for_zone_and_date(product TEXT, zone_nam
 AS $function$
 BEGIN
     RETURN QUERY EXECUTE FORMAT(
-        'WITH cbrfc_zone AS (
-            select geom, buffered_envelope from public.cbrfc_zone_buffer($1)
-        ),
+        'WITH raster_srid AS (
+            SELECT ST_SRID(%1$I.rast) AS epsg FROM %1$I LIMIT 1
+        ), 
+        cbrfc_zone AS (
+            SELECT ST_TRANSFORM(geom, raster_srid.epsg) as geom, 
+                ST_TRANSFORM(buffered_envelope, raster_srid.epsg) as transform_envelope
+                FROM public.cbrfc_zone_buffer($1), raster_srid
+        ), 
         product_pixels AS (
             SELECT (ST_PixelAsCentroids(
                 ST_CLIP(
-                    %I.rast,
-                    cbrfc_zone.buffered_envelope,
+                    %1$I.rast,
+                    cbrfc_zone.transform_envelope,
                     true
                 )
             )).*
-            FROM %I, cbrfc_zone
-            WHERE %I.swe_date = TO_DATE($2, ''YYYY-MM-DD'') 
-            AND ST_Intersects(%I.rast, cbrfc_zone.buffered_envelope)
+            FROM %1$I, cbrfc_zone
+            WHERE %1$I.swe_date = TO_DATE($2, ''YYYY-MM-DD'') 
+            AND ST_Intersects(
+                %1$I.rast, cbrfc_zone.transform_envelope
+            )
         )
         SELECT product_pixels.val, product_pixels.geom
         FROM product_pixels, cbrfc_zone
-        WHERE ST_WITHIN(product_pixels.geom, cbrfc_zone.geom)',
-        product, product, product, product
+        WHERE ST_WITHIN(
+            product_pixels.geom, cbrfc_zone.geom
+        )',
+        product
     ) USING zone_name, target_date;
 END
 $function$
